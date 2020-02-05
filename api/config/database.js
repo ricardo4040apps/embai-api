98fcb57cbbfc73b58b5b2f3c7af35bdc30af2a1f
Ricardo Plascencia <ricardoplascencia680@hotmail.com>
2020-02-05 14:16:27 -0600

    ip

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //database: 'mongodb://localhost/test', //18.189.157.105
-    database: 'mongodb://root:TXEA2wd2PEDM@localhost:27017/store-test',
-    secret: 'youtsecret'
+    database: 'mongodb://root:TXEA2wd2PEDM@18.189.157.105:27017/store-test',
+    secret: 'yoursecret'
 }
