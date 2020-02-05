aef468e15b02b971d9e247a5fd300d14d4c73044
Ricardo Plascencia <ricardoplascencia680@hotmail.com>
2020-02-05 13:05:06 -0600

    password

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //database: 'mongodb://localhost/test',
-    database: 'mongodb://root:bitnami@host:27017/store-test',
+    database: 'mongodb://root:TXEA2wd2PEDM@host:27017/store-test',
     secret: 'youtsecret'
 }
