9f403ffbea3955607e7fe3a60b8537b83162d78d
Ricardo Plascencia <ricardo@4040apps.com>
2020-02-06 13:39:27 -0600

    no message

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
     //database: 'mongodb://localhost/test',
-    database: 'mongodb://myTester:Entrar@18.189.157.105:27017/store-test',
+    database: 'mongodb://myTester:Entrar@18.189.157.105:27017/test',
+    //database: 'mongodb://myTester:Entrar@18.189.157.105:27017/store-test',
     secret: 'yoursecret'
 }
