be97f504d2cc8564e00ab7b39a70536555ce17b0
Ricardo Plascencia <ricardo@4040apps.com>
2020-02-19 12:04:26 -0600

    change database

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost/store-test',
-    //database: 'mongodb://myTester:Entrar@localhost:27017/store-test',
+    //database: 'mongodb://localhost/store-test',
+    database: 'mongodb://myTester:Entrar@localhost:27017/store-test',
     secret: 'yoursecret'
 }
