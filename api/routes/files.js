b4c12ed8cb740e4d99bd3dbcd86ade65c3d24fad
Ricardo Plascencia <ricardo@4040apps.com>
2020-03-12 13:18:09 -0600

    picture updates

--- api/routes/files.js
+++ api/routes/files.js
@@ -11,10 +11,12 @@ const upload = multer({
 var type = upload.single('file');
 var path = require('path')
 
+const User = require('../models/user');
+
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-                                C R U D
+                                ALL FILES
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
@@ -30,12 +32,12 @@ router.get("/:name", function (req, res, next) {
       return
     }
 
-/*
-    res.writeHead(200, {
-      "Content-Type": 'image/png'
-    });
-    */
-   res.writeHead(200);
+    /*
+      res.writeHead(200, {
+        "Content-Type": 'image/png'
+      });
+      */
+    res.writeHead(200);
 
     res.write(data, 'binary');
     res.end();
@@ -74,10 +76,73 @@ router.delete("/:name", function (req, res, next) {
 
 
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                                PROFILE PICTURES
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+router.get("/picture-profile/:name", function (req, res, next) {
+  //console.log(req)
+  // req.params.fileName ='undefined';
+  // console.log(req.header('userId'))
+
+  var src = './store/pictures/' + req.params.name
+
+  fs.readFile(src, "binary", function (err, data) {
+    if (err) {
+      console.error(err)
+      res.status(404).send('invalid map file');
+      return
+    }
+
+    /*
+        res.writeHead(200, {
+          "Content-Type": 'image/png'
+        });
+        */
+    res.writeHead(200);
+
+    res.write(data, 'binary');
+    res.end();
+  });
+});
+
+
+
+
+
+router.post('/picture-profile', type, function (req, res) {
+  const tmp_path = req.file.path;
+  console.log(req.header('userId'))
+  const hashName = getAvailableName(req.file.originalname);
+
+  var target_path = './store/pictures/' + hashName;
+
+  var src = fs.createReadStream(tmp_path);
+  var dest = fs.createWriteStream(target_path);
+  src.pipe(dest);
+
+  src.on('end', function () {
+    deleteFile(tmp_path);
+    updateUserPicture(req.header('userId'), hashName);
+    res.status(200).send(hashName)
+  });
+  src.on('error', function (err) { res.status(500).send(err) });
+});
+
+
+router.delete("/:name", function (req, res, next) {
+  //fileCtrl.deleteById(req, res, next);
+});
+
+
+
+
+
+
 var deleteFile = function (src) {
   fs.unlink(src, function (err) {
     if (err) throw err;
-    console.log('File deleted!');
+    // console.log('File deleted!');
   });
 }
 
@@ -92,6 +157,18 @@ var getAvailableName = function (src) {
 }
 
 
+var updateUserPicture = function (iserId = null, pictureName) {
+  const data = { "picture": pictureName };
+  if (iserId) {
+    User.update(iserId, data, (err, data) => {
+      if (err) {
+        console.error("route users put:", err)
+        return;
+      }
+      console.log("Picture Updated!")
+    });
+  }
+}
 
 
 module.exports = router;
