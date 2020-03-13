367d8021a440eb0aac85537eb0a29bff14c886e1
sinahiruiz <ruizrsinahi@gmail.com>
2020-03-13 11:31:15 -0600

    ajuste

--- api/routes/companies.js
+++ api/routes/companies.js
@@ -2,38 +2,31 @@ var express = require('express');
 var router = express.Router();
 const Company = require('../models/company');
 const passportMiddleware = require('../middlewares/passport');
-const   companyCtrl = require('../controllers/company');
+const companyCtrl = require('../controllers/company');
 
 /* GET users listing. */
 
 router.get('/', passportMiddleware, function(req, res, next) {
-  companyCtrl.get(req, res, next);
+    companyCtrl.get(req, res, next);
 });
 
 router.get('/:id', passportMiddleware, function(req, res, next) {
-  companyCtrl.getById(req, res, next);
+    companyCtrl.getById(req, res, next);
 });
 
 
 router.post('/', passportMiddleware, function(req, res, next) {
-  companyCtrl.create(req, res, next);
+    companyCtrl.create(req, res, next);
 });
 
 
 router.put('/:id', passportMiddleware, function(req, res, next) {
-  companyCtrl.update(req, res, next);
+    companyCtrl.update(req, res, next);
 });
 
 
 router.delete('/:id', passportMiddleware, function(req, res, next) {
-  companyCtrl.deleteById(req, res, next);
+    companyCtrl.deleteById(req, res, next);
 });
 
-
-
-
-
 module.exports = router;
-
-
-
