7467548f9e2483e7174f49d1068da0dc2e4044d3
Leo Estrada <xxkillerxx7@gmail.com>
2020-03-24 17:34:34 -0600

    Agregado relacion de ads con sus pictures

--- api/routes/files.js
+++ api/routes/files.js
@@ -6,12 +6,13 @@ const uuid = require('node-uuid');
 //const q = require('q')
 const multer = require('multer');
 const upload = multer({
-  dest: './store/pictures/' // this saves your file into a directory called "uploads"
+    dest: './store/pictures/' // this saves your file into a directory called "uploads"
 });
 var type = upload.single('file');
 var path = require('path')
 
 const User = require('../models/user');
+const ADS = require('../models/ads');
 
 
 
@@ -20,43 +21,43 @@ const User = require('../models/user');
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
-router.get("/:name", function (req, res, next) {
-  var src = './store/pictures/' + req.params.name
+router.get("/:name", function(req, res, next) {
+    var src = './store/pictures/' + req.params.name
 
-  fs.readFile(src, "binary", function (err, data) {
-    if (err) {
-      console.error(err)
-      res.status(404).send('invalid map file');
-      return
-    }
+    fs.readFile(src, "binary", function(err, data) {
+        if (err) {
+            console.error(err)
+            res.status(404).send('invalid map file');
+            return
+        }
 
-    res.writeHead(200);
-    res.write(data, 'binary');
-    res.end();
-  });
+        res.writeHead(200);
+        res.write(data, 'binary');
+        res.end();
+    });
 });
 
 
-router.post('/', type, function (req, res) {
-  const tmp_path = req.file.path;
-  const hashName = getAvailableName(req.file.originalname);
+router.post('/', type, function(req, res) {
+    const tmp_path = req.file.path;
+    const hashName = getAvailableName(req.file.originalname);
 
-  var target_path = './store/pictures/' + hashName;
+    var target_path = './store/pictures/' + hashName;
 
-  var src = fs.createReadStream(tmp_path);
-  var dest = fs.createWriteStream(target_path);
-  src.pipe(dest);
+    var src = fs.createReadStream(tmp_path);
+    var dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
 
-  src.on('end', function () {
-    deleteFile(tmp_path);
-    res.status(200).send(hashName)
-  });
-  src.on('error', function (err) { res.status(500).send(err) });
+    src.on('end', function() {
+        deleteFile(tmp_path);
+        res.status(200).send(hashName)
+    });
+    src.on('error', function(err) { res.status(500).send(err) });
 });
 
 
-router.delete("/:name", function (req, res, next) {
-  //fileCtrl.deleteById(req, res, next);
+router.delete("/:name", function(req, res, next) {
+    //fileCtrl.deleteById(req, res, next);
 });
 
 
@@ -68,40 +69,40 @@ router.delete("/:name", function (req, res, next) {
                                 PROFILE PICTURES
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-router.get("/picture-profile/:name", function (req, res, next) {
-  var src = './store/pictures/' + req.params.name
+router.get("/picture-profile/:name", function(req, res, next) {
+    var src = './store/pictures/' + req.params.name
 
-  fs.readFile(src, "binary", function (err, data) {
-    if (err) {
-      console.error(err)
-      res.status(404).send('invalid map file');
-      return
-    }
+    fs.readFile(src, "binary", function(err, data) {
+        if (err) {
+            console.error(err)
+            res.status(404).send('invalid map file');
+            return
+        }
 
-    res.writeHead(200);
-    res.write(data, 'binary');
-    res.end();
-  });
+        res.writeHead(200);
+        res.write(data, 'binary');
+        res.end();
+    });
 });
 
 
-router.post('/picture-profile', type, function (req, res) {
-  const tmp_path = req.file.path;
-  console.log(req.header('userId'))
-  const hashName = getAvailableName(req.file.originalname);
+router.post('/picture-profile', type, function(req, res) {
+    const tmp_path = req.file.path;
+    console.log(req.header('userId'))
+    const hashName = getAvailableName(req.file.originalname);
 
-  var target_path = './store/pictures/' + hashName;
+    var target_path = './store/pictures/' + hashName;
 
-  var src = fs.createReadStream(tmp_path);
-  var dest = fs.createWriteStream(target_path);
-  src.pipe(dest);
+    var src = fs.createReadStream(tmp_path);
+    var dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
 
-  src.on('end', function () {
-    deleteFile(tmp_path);
-    updateUserPicture(req.header('userId'), hashName);
-    res.status(200).send(hashName)
-  });
-  src.on('error', function (err) { res.status(500).send(err) });
+    src.on('end', function() {
+        deleteFile(tmp_path);
+        updateUserPicture(req.header('userId'), hashName);
+        res.status(200).send(hashName)
+    });
+    src.on('error', function(err) { res.status(500).send(err) });
 });
 
 
@@ -115,65 +116,66 @@ router.post('/picture-profile', type, function (req, res) {
                                 ADS PICTURES
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-router.get("/ads-pictures/:name", function (req, res, next) {
-  var src = './store/ads-pictures/' + req.params.name
+router.get("/ads-pictures/:name", function(req, res, next) {
+    var src = './store/ads-pictures/' + req.params.name
 
-  fs.readFile(src, "binary", function (err, data) {
-    if (err) {
-      console.error(err)
-      res.status(404).send('invalid map file');
-      return
-    }
+    fs.readFile(src, "binary", function(err, data) {
+        if (err) {
+            console.error(err)
+            res.status(404).send('invalid map file');
+            return
+        }
 
-    res.writeHead(200);
-    res.write(data, 'binary');
-    res.end();
-  });
+        res.writeHead(200);
+        res.write(data, 'binary');
+        res.end();
+    });
 });
 
 
-router.post('/ads-pictures', type, function (req, res) {
-  const tmp_path = req.file.path;
-  console.log(req.header('userId'))
-  const hashName = getAvailableName(req.file.originalname);
+router.post('/ads-pictures', type, function(req, res) {
+    const tmp_path = req.file.path;
+    console.log(req.header('userId'))
+    const hashName = getAvailableName(req.file.originalname);
 
-  var target_path = './store/ads-pictures/' + hashName;
+    var target_path = './store/ads-pictures/' + hashName;
 
-  var src = fs.createReadStream(tmp_path);
-  var dest = fs.createWriteStream(target_path);
-  src.pipe(dest);
+    var src = fs.createReadStream(tmp_path);
+    var dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
 
-  src.on('end', function () {
-    deleteFile(tmp_path);
-    //updateUserPicture(req.header('userId'), hashName);
-    res.status(200).send(hashName)
-  });
-  src.on('error', function (err) { res.status(500).send(err) });
-});
-
-
-
-
-
-router.delete("/:name", function (req, res, next) {
-  //fileCtrl.deleteById(req, res, next);
+    src.on('end', function() {
+        deleteFile(tmp_path);
+        //updateUserPicture(req.header('userId'), hashName);
+        updateADSPicture(req.header('userId'), hashName);
+        res.status(200).send(hashName)
+    });
+    src.on('error', function(err) { res.status(500).send(err) });
 });
 
 
 
 
 
+router.delete("/:name", function(req, res, next) {
+    //fileCtrl.deleteById(req, res, next);
+});
 
 
 
 
 
 
-var deleteFile = function (src) {
-  fs.unlink(src, function (err) {
-    if (err) throw err;
-    // console.log('File deleted!');
-  });
+
+
+
+
+
+var deleteFile = function(src) {
+    fs.unlink(src, function(err) {
+        if (err) throw err;
+        // console.log('File deleted!');
+    });
 }
 
 
@@ -183,27 +185,39 @@ var deleteFile = function (src) {
 
 // ???? is correct
 
-var getAvailableName = function (src) {
-  const ext = path.extname(src);
-  var fileName = uuid.v4() + ext;
-  if (fs.existsSync(fileName)) {
-    getAvailableName(src)
-  }
-  return fileName
+var getAvailableName = function(src) {
+    const ext = path.extname(src);
+    var fileName = uuid.v4() + ext;
+    if (fs.existsSync(fileName)) {
+        getAvailableName(src)
+    }
+    return fileName
 }
 
 
-var updateUserPicture = function (iserId = null, pictureName) {
-  const data = { "picture": pictureName };
-  if (iserId) {
-    User.update(iserId, data, (err, data) => {
-      if (err) {
-        console.error("route users put:", err)
-        return;
-      }
-      console.log("Picture Updated!")
-    });
-  }
+var updateUserPicture = function(iserId = null, pictureName) {
+    const data = { "picture": pictureName };
+    if (iserId) {
+        User.update(iserId, data, (err, data) => {
+            if (err) {
+                console.error("route users put:", err)
+                return;
+            }
+            console.log("Picture Updated!")
+        });
+    }
+}
+var updateADSPicture = function(iserId = null, pictureName) {
+    const data = { "picture": pictureName };
+    if (iserId) {
+        ADS.update(iserId, data, (err, data) => {
+            if (err) {
+                console.error("route ADS put:", err)
+                return;
+            }
+            console.log("Picture Updated!")
+        });
+    }
 }
 
 
