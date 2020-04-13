ab816e153803ca4f66615e51e8edd8251cc25748
Luis Fernando Martinez <luis@4040apps.com>
2020-04-13 18:22:52 -0500

    Informacion empresarial abierto

--- api/routes/informacion-empresarial.js
+++ api/routes/informacion-empresarial.js
@@ -7,7 +7,7 @@ const infoEmpCtrl = require('../controllers/informacion-empresarial');
 /* GET users listing. */
 
 
-router.get('/', passportMiddleware, function (req, res, next) {
+router.get('/', function (req, res, next) {
     infoEmpCtrl.getOwn(req, res, next);
 });
 
